a0da57d6a2a0d46119db8229406f00fd4e0cd161
ahmadsaleh963963 <ahmedsaleh405792@gmail.com>
2026-08-19 10:50:09 +0300

    hot fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -15,6 +17,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // قراءة المفاتيح من local.properties لضمان الأمان والرفع لـ GitHub
+        val properties = Properties()
+        val localPropertiesFile = project.rootProject.file("local.properties")
+        if (localPropertiesFile.exists()) {
+            properties.load(localPropertiesFile.inputStream())
+        }
+        val mapboxAccessToken = properties.getProperty("MAPBOX_ACCESS_TOKEN") ?: ""
+        resValue("string", "mapbox_access_token", mapboxAccessToken)
     }
 
     buildTypes {
@@ -30,6 +41,7 @@ android {
     }
     buildFeatures {
         compose = true
+        resValues = true
     }
 }
 
